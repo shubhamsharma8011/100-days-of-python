e1c8a0d810dd12df95c37423d6110635d323ce57
shubhamsharma8011 <shubhamsharma80111@gmail.com>
2026-07-04 14:04:39 +0545

    Day 1: Python basics

---
+++ Day01/Day01.py
@@ -0,0 +1,22 @@
+#Basic String Manipulation
+print("Hello World \nHello World")
+print("Hello " + "World")
+print()
+
+#Input Function
+print("Hello", input("What is your name?"))
+
+#Variables
+age=input("What is your age?")
+print("Hello",age)
+print()
+
+#Check the length of user input
+print(len(input("Enter to check the length: ")))
+
+#Swap the contents of glass1 and glass2 in 3 lines using only variables. Do not use the words "milk" or "juice".
+glass1 = "milk"
+glass2 = "juice"
+temp=glass1
+glass1=glass2
+glass2=temp
---
+++ Day01/Day01_Project.py
@@ -0,0 +1,4 @@
+#Band Name Generator
+city=input("which city did you grow up in? \n")
+pet=input("What is the name of your favorite pet? \n")
+print("Your band name could be: " + city + " " + pet)
